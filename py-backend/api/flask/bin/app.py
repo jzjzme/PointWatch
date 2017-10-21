7cf26b897faa2b80a4d82c98655e577789d2a67c
Kent Makishima <khmakishima@ucdavis.edu>
2017-10-21 16:34:21 -0700

    Changed search function name

--- py-backend/api/flask/bin/app.py
+++ py-backend/api/flask/bin/app.py
@@ -7,15 +7,15 @@ import json
 app = Flask(__name__)
 flight = PyFlight(api_key='AIzaSyBNFMQ6w1pnPwjAudfZw-iliHUucHRxqiA')
 
-@app.route('/flights', methods=['GET'])
-def flights():
+@app.route('/search', methods=['GET'])
+def search():
 	# examples parameters:
 	# adults = 1
 	# origin = 'SFO'
 	# price  = 'EUR500'
 	# dest   = 'LAS'
 	# date   = '2017-10-25'
-	#http://127.0.0.1:5000/flights?adults=1&origin=SFO&price=EUR500&dest=LAS&date=2017-10-25
+	#http://127.0.0.1:5000/search?adults=1&origin=SFO&price=EUR500&dest=LAS&date=2017-10-25
 
 	adults = request.args.get('adults')
 	origin = request.args.get('origin')
