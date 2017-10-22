01bd4d32fa72a185c345809bc81ac886cfc662cb
Kent <khmakishima@ucdavis.edu>
2017-10-21 19:08:31 -0700

    added files

--- py-backend/api/flask/bin/app.py
+++ py-backend/api/flask/bin/app.py
@@ -2,10 +2,12 @@
 from flask import Flask, jsonify
 from flask import request
 from pyflights import PyFlight
+from visa.helpers.visa_api_client import VisaAPIClient
+import datetime
 import json
 
 app = Flask(__name__)
-flight = PyFlight(api_key=' ')#no API key because public repositiory
+flight = PyFlight(api_key='AIzaSyD7VsS765wmLmGAzbnzit0sk_dB_wFULDI')#no API key because public repositiory
 
 @app.route('/search', methods=['GET']) #search for flights
 def search():
@@ -15,7 +17,7 @@ def search():
 	# price  = 'EUR500'
 	# dest   = 'LAS'
 	# date   = '2017-10-25'
-	#http://127.0.0.1:5000/search?adults=1&origin=SFO&price=EUR500&dest=LAS&date=2017-10-25
+	#http://127.0.0.1:5000/search?adults=1&origin=SFO&price=USD500&dest=LAX&date=2017-10-22
 
 	adults = request.args.get('adults')
 	origin = request.args.get('origin')
@@ -29,7 +31,7 @@ def search():
         'max_price': '%s'%price,
         'destination': '%s'%dest,
         'date': '%s'%date,
-        'solutions': 1
+        'solutions': 10
 	})
 	for results in search:
 	    output =  json.dumps(
@@ -42,11 +44,63 @@ def search():
 	        },
 	        sort_keys=True, indent=4, separators=(',',': '))
     
-	return output
+	return results
 
-@app.route('/transaction', methods=['GET']) #VISA direct transaction to 
+@app.route('/transaction', methods=['GET']) #VISA Direct transaction to 
+
+def screen():
+	self.visa_api_client = VisaAPIClient() #sample data, real data will be read via JSON
+		self.watch_list_inquiry = json.loads(''' { 
+			"acquirerCountryCode": "840",
+			"acquiringBin": "408999",
+			"address": {
+				"city": "San Francisco",
+				"cardIssuerCountryCode": "USA"
+				},
+				"referenceNumber": "000000004",
+				"name": "Jackie Zhang"
+				}''')
+
 def purchase():
+	date = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S") #visa direct transaction
+		self.visa_api_client = VisaAPIClient()
+		self.push_funds_request = json.loads('''{
+			"systemsTraceAuditNumber": 350420,
+			"retrievalReferenceNumber": "401010350420",
+			"localTransactionDateTime": "'''+ date + '''",
+			"acquiringBin": 409999,
+			"acquirerCountryCode": "101",
+			"senderAccountNumber": "1234567890123456",
+			"transactionCurrencyCode": "USD",
+			"senderName": "John Smith",
+			"senderCountryCode": "USA",
+			"senderAddress": "44 Market St.",
+			"senderCity": "San Francisco",
+			"senderStateCode": "CA",
+			"recipientName": "Adam Smith",
+			"recipientPrimaryAccountNumber": "4957030420210454",
+			"amount": "112.00",
+			"businessApplicationId": "AA",
+			"transactionIdentifier": 234234322342343,
+			"merchantCategoryCode": 6012,
+			"sourceOfFundsCode": "03",
+			"cardAcceptor": {
+				"name": "John Smith",
+				"terminalId": "13655392",
+				"idCode": "VMT200911026070",
+				"address": {
+					"state": "CA",
+					"county": "081",
+					"country": "USA",
+					"zipCode": "94105"
+					}
+				},
+			"feeProgramIndicator": "123"
+			}''')
 
+@app.route('/offers', methods=['GET']) #VISA Offers available to use
+
+def offers():
 
 if __name__ == '__main__':
 	app.run(debug=True)
