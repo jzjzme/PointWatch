708ad18b16fcede95ff5afff82b59ae7d39c968d
Kent <khmakishima@ucdavis.edu>
2017-10-21 19:48:10 -0700

    added functions to marketplace.py

---
+++ py-backend/marketplace/marketplace.py
@@ -0,0 +1,46 @@
+# marketplace.py
+# This sends all transactions to postgres database for pts.
+import sys
+import psycopg2
+
+conn = psycopg2.connect(database="marketplace", user="bwonymph") #enter database
+
+
+def sellpts(user, amount):
+	cur = conn.cursor()
+	sql = """INSERT INTO users(user) VALUES(amount) """
+	cur.execute(sql, user, amount)
+	conn.commit()
+
+def howmanypts(users):
+	cur = conn.cursor()
+	sql = """SELECT FROM amount FROM users(user)"""
+	cur.execute(sql, user)
+	pts = cur.fetchone()
+	conn.commit()
+	return pts
+
+def list_transaction(user, user2, amount, type):
+	cur = conn.cursor()
+	sql = """INSERT INTO buyer(user) 
+				"""
+def complete_transaction(transaction_id):
+	cur = conn.cursor()
+	sql = "SELECT user FROM transaction_id FROM transactions"
+	cur.execute(sql, transaction_id)
+	user1 = cur.fetchone()
+	sql = "SELECT user2 FROM transaction_id FROM transactions"
+	cur.(sql, transaction_id)
+	user1 = cur.fetchone()
+	sql = "SELECT cash FROM transaction_id FROM transactions"
+	cur.execute(sql, transaction_id)
+	cash = cur.fetchone()	
+	sql = "SELECT amount FROM transaction_id FROM transactions"
+	cur.execute(sql, transaction_id)
+	pts = cur.fetchone()
+	conn.commit()
+
+
+
+
+
