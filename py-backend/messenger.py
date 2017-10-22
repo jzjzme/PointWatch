8e7bd2fc728db7d84ed66f51c8daf0114fd80270
Kent <khmakishima@ucdavis.edu>
2017-10-21 21:23:40 -0700

    removed keys

--- py-backend/messenger.py
+++ py-backend/messenger.py
@@ -3,11 +3,7 @@
 #add to API
 import nexmo
 
-
-endpoint = "wss://h0j3zwoo.api.satori.com"
-appkey = "d3fE5A8bc1D9C2e8761DfCf7d6cab13a"
-
-client = nexmo.Client(key='d4f97f0e', secret='2e02d0a63e14517f')
+client = nexmo.Client(key=' ', secret=' ')
 
 response = client.send_message({'from': '12012413501', 'to': '19169908919', 'text': 'Someone wants to purchase your points! bit.ly92384239842'})
 
